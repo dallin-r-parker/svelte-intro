56c6e2c33d93f6a994720af4236c9f5c99114073
Dallin parker <13424017+dallin-r-parker@users.noreply.github.com>
2019-09-14 15:57:40 -0400

    clean up for showing versions

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script>
+  // Easy build in animations
   import { fade, fly } from "svelte/transition";
 
   let name;
@@ -6,38 +7,104 @@
   let rando;
   let randos = [];
 
-  // $: result = Math.round(rando * 100);
+  // sets variable to be one that is updated reactively
+  $: result = Math.round(rando * 100);
 
   function setRando() {
     rando = Math.random() * Math.random();
-    console.log(`rando: `, rando);
+
     randos = [...randos, rando];
   }
 
   function clear() {
     name = "";
   }
-
-  console.log(`randos: `, randos);
 </script>
 
 <style>
-  div {
+  .first {
+    width: 100%;
     display: flex;
     flex-direction: column;
     justify-content: center;
   }
+
+  .second {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    padding: 50px 0 50px 50px;
+  }
+
   h1 {
-    color: purple;
+    margin: 0;
+    padding: 0;
+    color: rgb(0, 30, 128);
+  }
+
+  h1 > span {
+    color: greenyellow;
+  }
+
+  strong {
+    color: red;
   }
 
   button {
     width: 100px;
   }
+
+  input {
+    width: 30%;
+  }
 </style>
 
-<div>
-  <h1>Hello {name ? name : ''}!</h1>
+<div class="first">
+  <div>
+    <h1>
+      This is the
+      <strong>FIRST</strong>
+      example
+    </h1>
+
+    <h1>
+      Hello
+      <span>{name ? name : ''}!</span>
+    </h1>
+    <p>the random number is</p>
+    <h3>{rando}</h3>
+
+    <button on:click={setRando}>Randomize</button>
+    <p>Your Score is {result ? result : 0}</p>
+
+    {#if result > 75}
+      <p transition:fade>Big winner! Top 25% 🎉🎉🎉</p>
+    {:else if result > 50}
+      <p>Decent, top 50% 😐</p>
+    {:else}
+      <p
+        in:fly={{ y: 1000, duration: 500 }}
+        out:fly={{ y: -500, duration: 500 }}>
+        Loser 💩💩💩
+      </p>
+    {/if}
+  </div>
+</div>
+
+<hr />
+
+<div class="second">
+  <h1>
+    This is the
+    <strong>SECOND</strong>
+    example
+  </h1>
+
+  <h1>
+    Hello
+    <span>{name ? name : '!'}</span>
+  </h1>
+  <!-- showing how to add event handlers on HTML Elements -->
   <input bind:value={name} on:blur={clear} />
   <button on:click={setRando}>PUSH</button>
 
@@ -49,24 +116,3 @@
     </ul>
   </div>
 </div>
-
-<!-- <div>
-  <h1>Hello {name}!</h1>
-  <p>the random number is</p>
-  <h3>{rando}</h3>
-
-  <hr />
-
-  <button on:click={setRando}>Randomize</button>
-  <p>Your Score is {result ? result : 0}</p>
-
-  {#if result > 75}
-    <p transition:fade>Big winner! Top 25% 🎉🎉🎉</p>
-  {:else if result > 50}
-    <p>Decent, top 50% 😐</p>
-  {:else}
-    <p in:fly={{ y: 1000, duration: 500 }} out:fly={{ y: -500, duration: 500 }}>
-      Loser 💩💩💩
-    </p>
-  {/if}
-</div> -->
